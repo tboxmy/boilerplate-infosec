e93f3eb7fb127cf3bcb4d462ce061987bdde6c3a
tboxmy <t_boxmy@yahoo.com>
2023-09-20 17:57:23 +0800

    add helmet deny for iframe

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ const helmet = require('helmet')
 module.exports = app
 const api = require('./server.js')
 app.use(express.static('public'))
-app.use(helmet.frameguard('deny'))
+app.use(helmet.frameguard({ action: 'deny' }))
 app.use(helmet.hidePoweredBy())
 app.disable('strict-transport-security')
 app.use('/_api', api)
